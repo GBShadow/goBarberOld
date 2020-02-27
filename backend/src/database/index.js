d138d623892a7fcd5de5d45b2cd258b405888ae5
GBShadow <gbs.shadow@gmail.com>
2020-02-27 17:00:49 -0300

    Create appointments

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,10 +1,12 @@
 import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
+import File from '../app/models/File';
+import Appointment from '../app/models/Appointment';
 
 import databaseConfig from '../config/database';
 
-const models = [User];
+const models = [User, File, Appointment];
 
 class Database {
   constructor() {
@@ -12,9 +14,11 @@ class Database {
   }
 
   init() {
-    this.connction = new Sequelize(databaseConfig);
+    this.connection = new Sequelize(databaseConfig);
 
-    models.map(model => model.init(this.connction));
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 }
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,6 +4,9 @@ import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddlewares from './app/middlewares/auth';
 
@@ -17,8 +20,10 @@ routes.use(authMiddlewares);
 
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.get('/providers', ProviderController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
+
+routes.post('/appointments', AppointmentController.store);
 
 export default routes;
