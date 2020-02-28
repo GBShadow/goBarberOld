2a58da8e548c21ca3e44e192249ef7cc24306b18
GBShadow <gbs.shadow@gmail.com>
2020-02-28 18:12:37 -0300

    List schedule of provider

--- backend/src/app/controllers/AppointmentController.js
+++ backend/src/app/controllers/AppointmentController.js
@@ -6,10 +6,14 @@ import Appointment from '../models/Appointment';
 
 class AppointmentController {
   async index(req, res) {
+    const { page = 1 } = req.query;
+
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
       order: ['date'],
       attributes: ['id', 'date'],
+      limit: 20,
+      offset: (page - 1) * 20,
       include: [
         {
           model: User,
@@ -19,7 +23,7 @@ class AppointmentController {
             {
               model: File,
               as: 'avatar',
-              attributes: ['id', 'url'],
+              attributes: ['id', 'path', 'url'],
             },
           ],
         },
