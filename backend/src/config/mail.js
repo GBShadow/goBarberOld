46b0c37ec97a36144b623cb194cebfa5503b640a
GBShadow <gbs.shadow@gmail.com>
2020-03-11 17:16:03 -0300

    feature/Send email of cancellation

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '18d2f18f3db672',
+    pass: '5b52279e173f02',
+  },
+  default: {
+    from: 'Equipe goBarber <noreply@gobarber.com.br>',
+  },
+};
