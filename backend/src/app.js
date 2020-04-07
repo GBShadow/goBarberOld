f01762ea098bb8d2845a8abed4a341268fb420c7
GBShadow <gbs.shadow@gmail.com>
2020-04-06 21:56:50 -0300

    Fixed backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import express from 'express';
 import * as Sentry from '@sentry/node';
 import Youch from 'youch';
+import cors from 'cors';
 import path from 'path';
 import 'express-async-errors';
 import routes from './routes';
@@ -23,6 +24,7 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
--- backend/src/config/sentry.js
+++ backend/src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-  dsn: 'https://acb6b8955137416e878235fcf203331b@sentry.io/5173903',
+  dsn: process.env.SENTRY_DNS,
 };
